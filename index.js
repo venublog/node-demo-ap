12424a6dadfa65b8f535780cd71a762abfcc957e
venublog <39304238+venublog@users.noreply.github.com>
2018-06-05 07:41:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World! 2 new test');
+  res.send('!Hello World! 2 test');
 });
 
 var server = app.listen(3000, function () {
